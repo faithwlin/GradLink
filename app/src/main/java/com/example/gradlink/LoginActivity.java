19e495832a4c4948f0c37fa11fceba31598bd36d
Amanda <amandaali@live.com.au>
2019-07-13 19:20:52 +1000

    Facebook Login API

---
+++ app/src/main/java/com/example/gradlink/LoginActivity.java
@@ -0,0 +1,116 @@
+package com.example.gradlink;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton fbLoginButton;
+    private TextView emailText, nameText;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        fbLoginButton = findViewById(R.id.fbLoginButton);
+        emailText = findViewById(R.id.emailText);
+        nameText = findViewById(R.id.nameText);
+
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginButton.setReadPermissions(Arrays.asList("email","public_profile"));
+
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken==null){
+                nameText.setText(" ");
+                emailText.setText(" ");
+                Toast.makeText(LoginActivity.this,"User logged out", Toast.LENGTH_LONG).show();
+            }
+            else
+                loaduserProfile(currentAccessToken);
+        }
+    };
+    private void loaduserProfile(AccessToken newAccessToken)
+    {
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response)
+            {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+                    String image_url = "https://graph.facebook.com/"+id+ "/picture?type=normal";
+
+                    emailText.setText(email);
+                    nameText.setText(first_name +" "+last_name);
+                    RequestOptions requestOptions = new RequestOptions();
+                    requestOptions.dontAnimate();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name, last_name, email, id");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+    private void checkLoginStatus () {
+        if(AccessToken.getCurrentAccessToken()!=null)
+        {
+            loaduserProfile(AccessToken.getCurrentAccessToken());
+        }
+    }
+}
