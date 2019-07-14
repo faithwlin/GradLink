baa911b89e11c2f570d3987b11ee5f0a95a673a9
Faith <faithlin20@gmail.com>
2019-07-14 10:30:17 +1000

    splash screen

---
+++ app/src/main/java/com/example/gradlink/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.example.gradlink;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        iv = (ImageView) findViewById(R.id.splashscreenlogo);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.transition);
+        iv.startAnimation(animation);
+    }
+}
