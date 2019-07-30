5606c7d8143e0794b581780b9254ebf23ee4187b
Amanda <amandaali@live.com.au>
2019-07-30 16:21:44 +1000

    test

--- app/src/main/java/com/example/gradlink/LoginActivity.java
+++ app/src/main/java/com/example/gradlink/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.gradlink;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,101 +22,148 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+    private ImageButton SignOut;
+    private ImageButton SignIn;
+    private ImageButton Enter;
+    private TextView Name,Email;
+    private ImageView logo;
 
-    private LoginButton fbLoginButton;
-    private TextView emailText, nameText;
-    private CallbackManager callbackManager;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Define objects
         setContentView(R.layout.activity_login);
+        SignOut = (ImageButton)findViewById(R.id.logoutBtn);
+        SignIn = (ImageButton)findViewById(R.id.loginBtn);
+        Enter = (ImageButton)findViewById(R.id.enterBtn);
+        Name = (TextView)findViewById(R.id.name);
+        Email = (TextView)findViewById(R.id.email);
 
-        fbLoginButton = findViewById(R.id.fbLoginButton);
-        fbLoginButton.setBackgroundResource(R.drawable.fbsignup);
-        fbLoginButton.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
-        emailText = findViewById(R.id.emailText);
-        nameText = findViewById(R.id.nameText);
+        //Set listeners for buttons
+        SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
+        Enter.setOnClickListener(this);
 
-        callbackManager = CallbackManager.Factory.create();
-        fbLoginButton.setReadPermissions(Arrays.asList("email","public_profile"));
+        //Hide visibility of user detail containers
+        Name.setVisibility(View.GONE);
+        Email.setVisibility(View.GONE);
+        SignOut.setVisibility(View.GONE);
+        Enter.setVisibility(View.GONE);
 
-        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        //Initialise Google API Client and add API
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
+    }
+
+    //Identify which button is clicked
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.loginBtn:
+                signIn();
+                break;
+            case R.id.logoutBtn:
+                signOut();
+                break;
+            case R.id.enterBtn:
+                enter();
+                break;
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    //Declare methods for buttons
+    private void enter()
+    {
+
+        startActivity(new Intent (LoginActivity.this, MainActivity.class));
+
+    }
+
+    private void signIn()
+    {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent,REQ_CODE);
+    }
+    private void signOut()
+    {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
             }
         });
+    }
+    //Declare method to handle sign in results and display to user
+    private void handleResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+
+
+            Name.setText(name);
+            Email.setText(email);
+            updateUI(true);
+
+
+        }
+        else
+        {
+            updateUI(false);
+        }
+    }
+
+    //Declare method to unhide/show user details
+    private void updateUI(boolean isLogin)
+    {
+        if(isLogin)
+        {
+            Name.setVisibility(View.VISIBLE);
+            Email.setVisibility(View.VISIBLE);
+            SignOut.setVisibility(View.VISIBLE);
+            Enter.setVisibility(View.VISIBLE);
+            SignIn.setVisibility(View.GONE);
+        }
+        else
+        {
+            Name.setVisibility(View.GONE);
+            Email.setVisibility(View.GONE);
+            SignOut.setVisibility(View.GONE);
+            Enter.setVisibility(View.GONE);
+            SignIn.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
-    }
-    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
-        @Override
-        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
-            if (currentAccessToken==null){
-                nameText.setText(" ");
-                emailText.setText(" ");
-                Toast.makeText(LoginActivity.this,"User logged out", Toast.LENGTH_LONG).show();
-            }
-            else
-                loaduserProfile(currentAccessToken);
-        }
-    };
-    private void loaduserProfile(AccessToken newAccessToken)
-    {
-        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
-            @Override
-            public void onCompleted(JSONObject object, GraphResponse response)
-            {
-                try {
-                    String first_name = object.getString("first_name");
-                    String last_name = object.getString("last_name");
-                    String email = object.getString("email");
-                    String id = object.getString("id");
-                    String image_url = "https://graph.facebook.com/"+id+ "/picture?type=normal";
-
-                    emailText.setText(email);
-                    nameText.setText(first_name +" "+last_name);
-                    RequestOptions requestOptions = new RequestOptions();
-                    requestOptions.dontAnimate();
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
-        Bundle parameters = new Bundle();
-        parameters.putString("fields", "first_name, last_name, email, id");
-        request.setParameters(parameters);
-        request.executeAsync();
-    }
-    private void checkLoginStatus () {
-        if(AccessToken.getCurrentAccessToken()!=null)
-        {
-            loaduserProfile(AccessToken.getCurrentAccessToken());
+        if(requestCode==REQ_CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
         }
     }
 }
