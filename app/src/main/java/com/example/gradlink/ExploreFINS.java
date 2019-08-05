f24074caed32bf3aa199b3da88031df667bc872c
Faith <faithlin20@gmail.com>
2019-08-06 00:00:37 +1000

    Youtube for infs, fins, acct, mark

--- app/src/main/java/com/example/gradlink/ExploreFINS.java
+++ app/src/main/java/com/example/gradlink/ExploreFINS.java
@@ -2,12 +2,54 @@ package com.example.gradlink;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class ExploreFINS extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class ExploreFINS extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    static final String GOOGLE_API_KEY = "AIzaSyC8N9d1IKrSrZ7N_r_VY8A86rErk64OlBY";
+    static final String YOUTUBE_VIDEO = "53Lv0t3dZBc";
+    final int REQUEST_CODE = 1;
+
+    Toolbar toolbar;
+
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore_fins);
+
+        //Define YouTubePlayerView
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.player);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ExploreFINS.this.onBackPressed();
+            }
+        });
+        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+        toolbar.setTitle("Finance");
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.cueVideo(YOUTUBE_VIDEO);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        }
     }
 }
--- app/src/main/java/com/example/gradlink/ExploreINFS.java
+++ app/src/main/java/com/example/gradlink/ExploreINFS.java
@@ -2,12 +2,54 @@ package com.example.gradlink;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class ExploreINFS extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class ExploreINFS extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    static final String GOOGLE_API_KEY = "AIzaSyC8N9d1IKrSrZ7N_r_VY8A86rErk64OlBY";
+    static final String YOUTUBE_VIDEO = "fMCJFTX36J4";
+    final int REQUEST_CODE = 1;
+
+    Toolbar toolbar;
+
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore_infs);
+
+        //Define YouTubePlayerView
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.player);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ExploreINFS.this.onBackPressed();
+            }
+        });
+        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+        toolbar.setTitle("Information Systems");
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.cueVideo(YOUTUBE_VIDEO);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        }
     }
 }
