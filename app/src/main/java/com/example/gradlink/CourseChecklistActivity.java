c9b06e8d25eafed8b93ecafbf3bbcb7f1aa04c73
Calvin <calvin.zeng4@gmail.com>
2019-08-12 21:55:30 +1000

    flippable badges

--- app/src/main/java/com/example/gradlink/CourseChecklistActivity.java
+++ app/src/main/java/com/example/gradlink/CourseChecklistActivity.java
@@ -3,6 +3,8 @@ package com.example.gradlink;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wajahatkarim3.easyflipview.EasyFlipView;
+
 public class CourseChecklistActivity extends AppCompatActivity {
 
     @Override
@@ -12,5 +14,10 @@ public class CourseChecklistActivity extends AppCompatActivity {
 
         //Slide in and out animation for activity transition
         overridePendingTransition(R.anim.animation_enter, R.anim.animation_leave);
+
+        final EasyFlipView easyFlipView1 = (EasyFlipView) findViewById(R.id.easyFlipView1);
+        easyFlipView1.setToVerticalType();
+        easyFlipView1.setFlipTypeFromBack();
+
     }
 }
